5dffa649380dafc00605f09b95d6c93be3a8a337
unknown <abdulazisbur@gmail.com>
2024-03-09 18:15:53 +0700

    lets go

---
+++ biodata.py
@@ -0,0 +1,19 @@
+# membuait variable
+nama = "azis"
+umur = 21
+semester = 2
+kelas = "TI23B"
+jurusan = "Teknik informatika"
+quote = "my life my choice"
+married = False
+adalahPerempuan = False
+hobi = ["nonton", "main game"]
+
+
+print("nama saya", nama)
+print("Semester", semester)
+print("Kelas", kelas)
+print("umur saya", umur)
+print("Jurusan", jurusan)
+print(f"aku suka {hobi[0]} dan {hobi[1]}.")
+print("quote", quote)
